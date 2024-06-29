943f426c427500672e88d7daf3bbc752d5d03726
Matías Neira <matiasneirab@gmail.com>
2024-06-29 14:04:43 -0300

    add edit product

--- controllers/productos.js
+++ controllers/productos.js
@@ -9,6 +9,16 @@ exports.GetProduct = async (req, res) => {
         res.status(400).json({ ok: false, message: 'Ha ocurrido un error' });
     }
 }
+exports.GetProducts = async (req, res) => {
+    try {
+        const sku = req.params.sku;
+        const products = await Product.findById(sku);
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ ok: false, message: 'Ha ocurrido un error' });
+    }
+}
 
 exports.NewProduct = (req, res) => {
     console.log(req.body);
